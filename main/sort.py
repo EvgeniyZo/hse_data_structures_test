ff8b1540c2415fef9ee1a3a5d9d4424d81518166
Yuriy Kotsubinskiy <42349763+vir1ibus@users.noreply.github.com>
2024-04-03 20:13:29 +0300

    Second task

--- main/sort.py
+++ main/sort.py
@@ -1,3 +1,13 @@
 
 def sort(a):
-    return
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(1, len(a)):
+            if a[i] < a[i-1]:
+                tmp = a[i]
+                a[i] = a[i-1]
+                a[i-1] = tmp
+                sorted = False
+    return a
+    
