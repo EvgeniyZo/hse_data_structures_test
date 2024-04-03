0a5662d6951172cec5295405bce9da3c018b2e3b
Windows 10 <student10@std.nnov.corp.local>
2024-04-03 20:01:33 +0300

    1 task

--- main/simplemath.py
+++ main/simplemath.py
@@ -1,3 +1,6 @@
+import math
 
 def f_me(x):
-    return 0 # changeme
+    if type(x) is not float and type(x) is not int or math.isinf(x) or x is None:
+        return None
+    return 25 * x + 10 # changeme
