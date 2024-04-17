f894c0bc20d20741d34dca51b4016a1c2d2fe49d
GooD-NTS <good.nts@gmail.com>
2024-04-17 19:16:26 +0300

    fib

--- main/fib.py
+++ main/fib.py
@@ -3,4 +3,12 @@
 
 # Получить число Фибоначчи по его номеру
 def fib(n):
-    return 0
+    a = [1, 1] 
+      
+    for i in range(2, n): 
+        a.append(a[i-1] + a[i-2]) 
+      
+    for i in range(n): 
+        print(a[i]) 
+     
+    return a
