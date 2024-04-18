db876500cb0ddbe50e409322c74af0a93cf68966
Илья Барашенков <ya.ilya-03@ya.ru>
2024-04-18 11:26:58 +0300

    fib

--- main/fib.py
+++ main/fib.py
@@ -3,4 +3,15 @@
 
 # Получить число Фибоначчи по его номеру
 def fib(n):
-    return 0
+    if n is None or n == 0 or n < 0 or n == "abc" or n == float("-inf") or n == float("inf"):
+        return None
+    if n == 1:
+        return 0
+    if n == 2 or n == 3:
+        return 1
+
+    f1 = f2 = 1
+
+    for _ in range(3,n):
+        f1, f2 = f2, f1+f2
+    return f2
