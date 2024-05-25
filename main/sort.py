5f606a79448d90257bd0435ab86c5ee424e9f4e7
ilEugenD <105589224+ilEugenD@users.noreply.github.com>
2024-05-25 07:51:06 +0300

    Update sort.py

--- main/sort.py
+++ main/sort.py
@@ -2,9 +2,9 @@ def quicksort(arr):
     if len(arr) < 2:
         return arr
     pivot = arr[0]
-    less = [i for i in arr[1:] if i < pivot]
+    less = [i for i in arr[1:] if i <= pivot]
     greater = [i for i in arr[1:] if i > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
 
 def sort(a):
-    return
-    return quicksort(less) + [pivot] + quicksort(greater)
+    return quicksort(a)
