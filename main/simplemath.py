5f32773d9b80bb4fa8e057dcec3799b850aa5a46
ilEugenD <105589224+ilEugenD@users.noreply.github.com>
2024-05-25 07:57:08 +0300

    Update simplemath.py

--- main/simplemath.py
+++ main/simplemath.py
@@ -1,7 +1,4 @@
-
 def f_me(x):
-    return 0 # changeme
-    if not isinstance(x, (int, float)) or x in [float("inf"), float("-inf"), None]:
-        return None
-
-    return x * 25 + 10
+    if isinstance(x, (int, float)) and abs(x) < float("inf"):
+        return x * 25 + 10
+    return None
