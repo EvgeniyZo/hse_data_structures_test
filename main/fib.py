a334b53e0d7cd2de1b9def80329ce18168904763
ilEugenD <105589224+ilEugenD@users.noreply.github.com>
2024-05-25 07:57:25 +0300

    Update fib.py

--- main/fib.py
+++ main/fib.py
@@ -1,18 +1,14 @@
-# Числа Фибоначчи — числовой ряд, при котором каждое последующее число равно сумме двух предыдущих
-
-# Получить число Фибоначчи по его номеру
 def fib(n):
-    # Проверка входных данных
-    if not isinstance(n, int) or n < 1:
+    if n == None or n == 0 or n == "abc" or n == float("-inf") or n == float("inf") or n < 0:
         return None
     if n == 1:
-        return 0
-    if n == 2:
-        return 1
+         return 0
+    if n == 2 or n == 3:
+         return 1
+    
+    fib1 = fib2 = 1
 
-    # Вычисление n-го числа Фибоначчи
-    a, b = 0, 1
-    for _ in range(2, n):
-        a, b = b, a + b
-
-    return b
+    for _ in range(3, n):
+            fib1 , fib2 = fib2, fib1 + fib2
+            
+    return fib2
