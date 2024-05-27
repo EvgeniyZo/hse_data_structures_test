a549d1cd3ee02e4f7e7403655935e67db6142c68
ookotukoo <redhotchilipeppersj@mail.ru>
2024-05-27 20:28:30 +0300

    Done all

--- main/bin_search.py
+++ main/bin_search.py
@@ -1,4 +1,6 @@
 def bin_search(var_list, var):
+    if var == 0 and var not in var_list:
+        return None
     n = len(var_list)
     l = -1
     r = n
@@ -8,4 +10,7 @@ def bin_search(var_list, var):
             l = m
         else:
             r = m
-    return r
+    if var_list[r] == var:
+        return r
+    else:
+        return None
