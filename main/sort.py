ec1e691a97d65a4b4e7721e8ade3573448be7563
Anastasia <99526512+zutilda@users.noreply.github.com>
2024-05-27 22:27:44 +0300

    Update sort.py

--- main/sort.py
+++ main/sort.py
@@ -1,3 +1,5 @@
 
-def sort(a):
-    return
+def sort(arr):
+    if not all(isinstance(x, (int, float)) for x in arr):
+        raise ValueError("All elements must be numbers.")
+    return sorted(arr)
