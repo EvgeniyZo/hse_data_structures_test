106742f5067448a86366fed437d2e5414a5fb2c6
ookotukoo <redhotchilipeppersj@mail.ru>
2024-05-27 17:29:12 +0300

    Done all

--- main/sort.py
+++ main/sort.py
@@ -1,3 +1,12 @@
 
 def sort(a):
-    return
+    N = len(a)
+    i = 0
+    while i < N - 1:
+        j = 0
+        while j < N - 1 - i:
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+            j += 1
+        i += 1
+    return a
