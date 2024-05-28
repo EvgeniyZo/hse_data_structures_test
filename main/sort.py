d3076401eea9c66115558bdc9bb269511675d81a
ABK1ng <165947439+ABK1ng@users.noreply.github.com>
2024-05-28 20:52:05 +0300

    Update sort.py

--- main/sort.py
+++ main/sort.py
@@ -1,5 +1,7 @@
-def sort(n):
-	for j in range(0, n-i-1):
-		if a[j] > a[j+1]:
-			a[j], a[j+1] = a[j+1], a[j]
-return n
+def sort(a):
+	n = len(a)
+	for i in range(n):
+		for j in range(0, n-i-1):
+			if a[j] > a[j+1]:
+				a[j], a[j+1] = a[j+1], a[j]
+return a
