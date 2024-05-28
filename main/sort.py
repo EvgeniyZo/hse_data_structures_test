4f3d2aad1933cf734c9dc629c2ae55ec259d3ebf
ABK1ng <165947439+ABK1ng@users.noreply.github.com>
2024-05-28 19:58:54 +0300

    Update sort.py

--- main/sort.py
+++ main/sort.py
@@ -1,7 +1,7 @@
 def quicksort(arr):
    for j in range(0, n-i-1):
-			if a[j] > a[j+1]:
-				a[j], a[j+1] = a[j+1], a[j]
+	   if a[j] > a[j+1]:
+		   a[j], a[j+1] = a[j+1], a[j]
 
 def sort(a):
     return quicksort(a)
