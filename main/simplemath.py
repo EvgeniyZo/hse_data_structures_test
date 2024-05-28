df892158a68b8b5f722b88c1f7185a6b302d6864
Елена Писарева <pisarelena@yandex.ru>
2024-05-28 21:58:33 +0300

    first commit

--- main/simplemath.py
+++ main/simplemath.py
@@ -1,3 +1,9 @@
+import math
+
 
 def f_me(x):
-    return 0 # changeme
+    if isinstance(x, (int, float)):
+        if not math.isinf(x):
+            return x * 25 + 10
+    return None
+
--- main/sort.py
+++ main/sort.py
@@ -1,3 +1,7 @@
+def sort(nums):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - 1 - i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
-def sort(a):
-    return
+    return nums
