7816e884a6e8a1e756c35ab0f8f46905816616d5
ABK1ng <165947439+ABK1ng@users.noreply.github.com>
2024-05-28 20:38:32 +0300

    Update fib.py

--- main/fib.py
+++ main/fib.py
@@ -2,13 +2,12 @@ def fib(n):
     if n == None or n == 0 or n == "abc" or n == float("-inf") or n == float("inf") or n < 0:
         return None
     if n == 1:
-         return 0
+                 return 0
     if n == 2 or n == 3:
          return 1
 
     fib1 = fib2 = 1
-
-    for _ in range(3, n):
+        for _ in range(3, n):
             fib1 , fib2 = fib2, fib1 + fib2
 
     return fib2
