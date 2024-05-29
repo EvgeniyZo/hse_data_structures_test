d5c41b1812fa78b0f0eef0a2579905ae7e686ae8
Anastasia <99526512+zutilda@users.noreply.github.com>
2024-05-29 12:38:39 +0300

    Update sort.py

--- main/sort.py
+++ main/sort.py
@@ -1,5 +1,12 @@
 
 def sort(arr):
-    if not all(isinstance(x, (int, float)) for x in arr):
-        raise ValueError("All elements must be numbers.")
-    return sorted(arr)
+     n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
