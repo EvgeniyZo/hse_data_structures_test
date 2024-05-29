85b58571fe41637841d73e19c6e40176fdb183ca
CaptainBloodborne <vrail30@gmail.com>
2024-05-29 18:53:56 +0300

    add test

--- tests/test_square_sort.py
+++ tests/test_square_sort.py
@@ -4,3 +4,8 @@ def test_sort():
     arr = [5, 1, 7, 8, 5, 3, 4, 2]
     sort(arr)
     assert arr == [1, 2, 3, 4, 5, 5, 7, 8]
+
+    arr = [5, 1, 7, 8, 3, 4, 2]
+    sort(arr)
+
+    assert arr == [1, 2, 3, 4, 5, 7, 8]
